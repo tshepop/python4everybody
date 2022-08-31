1b540e4d9f002641fce3bcd3c67eed42cc9a6f1b
Tshepo <potelo.tshepo@gmail.com>
2022-08-31 21:56:31 +0200

    Renamed the file

---
+++ chapter-13/exercise-13.1.py
@@ -0,0 +1,28 @@
+import urllib.request
+import urllib.parse
+import urllib.error
+from geopy.geocoders import Nominatim
+import json
+
+"""
+I chose to use the Geopy library for geolocation, instead of the google maps API geocode.
+Geopy is able to retrieve different services that provide APIs and implements accessing these
+different services in a single package. OpenStreetMap is open source.
+
+install geopy on Ubuntu
+sudo apt-get install geopy
+
+"""
+
+geolocator = Nominatim(user_agent="my_locations")
+
+address_inp = input("Enter latitude and longitude: ")
+
+location = geolocator.reverse(address_inp)
+loc_dict = location.raw
+
+# convert python obj into json string
+info = json.dumps(loc_dict, indent=4)
+# print(info)
+print("-" * 20)
+print("Country Code:", str(loc_dict["address"]["country_code"]).upper())
