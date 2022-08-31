eb488bfaebd3c7e7f73a2eddc5db68a7a25c8676
Tshepo <potelo.tshepo@gmail.com>
2022-08-31 21:00:27 +0200

    Returned a dictionary from location object

--- chapter-13/geojson.py
+++ chapter-13/geojson.py
@@ -8,8 +8,16 @@ import json
 I chose to use the Geopy library for geolocation, instead of the google maps API geocode.
 Geopy is able to retrieve different services that provide APIs and implements accessing these
 different services in a single package. OpenStreetMap is open source.
+
+install geopy on Ubuntu
+sudo apt-get install geopy
+
 """
 
 geolocator = Nominatim(user_agent="my_locations")
 
-address_inp = input("Enter address: ")
+address_inp = input("Enter latitude and longitude: ")
+
+location = geolocator.reverse(address_inp)
+loc_dict = location.raw
+print(type(loc_dict))
